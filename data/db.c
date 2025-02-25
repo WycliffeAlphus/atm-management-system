689b64969b0541fc87af6ea012b7c466d9c879d4
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-25 22:20:16 +0300

    feat:add db initialization

---
+++ data/db.c
@@ -0,0 +1,34 @@
+#include "db.h"
+#include <stdio.h>
+
+int init_database(sqlite3 *db){
+
+    char *err_msg = 0;
+    const char *sql = 
+       "CREATE TABLE IF NOT EXISTS users ("
+            "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+            "name TEXT UNIQUE NOT NULL,"
+            "password TEXT NOT NULL"
+        ");"
+        "CREATE TABLE IF NOT EXISTS accounts ("
+            "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+            "user_id INTEGER,"
+            "user_name TEXT,"
+            "account_id INTEGER,"
+            "date_created TEXT,"
+            "country TEXT,"
+            "phone TEXT,"
+            "balance REAL,"
+            "type TEXT,"
+            "FOREIGN KEY (user_id) REFERENCES users(id)"
+        ");";
+
+    int rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+
+    if (rc != SQLITE_OK){
+        fprintf(stderr, "SQL error:%s\n", err_msg);
+        sqlite3_free(err_msg);
+        return 1;
+    }    
+    return 0;
+}
