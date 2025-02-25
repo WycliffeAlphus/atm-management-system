b430d803ea2cc65356d3eefbe544701fd1e5a940
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-26 00:46:31 +0300

    feat:add clear screan and message pause effect

--- src/main.c
+++ src/main.c
@@ -4,14 +4,13 @@ int main() {
     sqlite3 *db;
     int rc;
 
-    
     rc = sqlite3_open("data/atm.db", &db);
     if (rc != SQLITE_OK) {
         fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        PAUSE_DISPLAY(); 
         return 1;
     }
 
-    
     if (init_database(db) != 0) {
         sqlite3_close(db);
         return 1;
@@ -21,35 +20,40 @@ int main() {
     int logged_in_user_id = -1;
 
     while (1) {
-        printf("\n=== ATM Management System ===\n");
+        CLEAR_SCREEN();
+        printf("=== ATM Management System ===\n");
         printf("1. Login\n");
         printf("2. Register\n");
         printf("3. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        getchar(); 
+        getchar();
 
         switch (choice) {
             case 1:
                 logged_in_user_id = login_user(db);
                 if (logged_in_user_id >= 0) {
-                    printf("Login successful!\n");
                     account_menu(db, logged_in_user_id);
-                    logged_in_user_id = -1; 
+                    logged_in_user_id = -1;
                 }
                 break;
             case 2:
-                // register_user(db);
+                register_user(db);
                 break;
             case 3:
+                CLEAR_SCREEN();
                 printf("Goodbye!\n");
+                PAUSE_DISPLAY(); 
                 sqlite3_close(db);
                 return 0;
             default:
+                CLEAR_SCREEN();
                 printf("Invalid choice. Please try again.\n");
+                PAUSE_DISPLAY(); 
+                break;
         }
     }
 
-    sqlite3_close(db); 
+    sqlite3_close(db);
     return 0;
 }
