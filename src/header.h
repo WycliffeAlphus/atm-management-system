c780fc1e20479d1026dbe51627fdf54db1be106c
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-25 23:07:33 +0300

    add account_menu to header

--- src/header.h
+++ src/header.h
@@ -7,7 +7,11 @@
 #include <sqlite3.h>
 #include "../data/db.h"
 
+// Authentication functions (auth.c)
 int login_user(sqlite3 *db);
 void register_user(sqlite3 *db);
+
+// Account management functions (accounts.c)
+void account_menu(sqlite3 *db, int user_id);
 
 #endif
