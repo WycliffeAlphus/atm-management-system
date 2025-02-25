205814a1c84c90cf499d8da73ecaf422962a7044
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-26 00:45:39 +0300

    feat:define clear screen and pause

--- src/header.h
+++ src/header.h
@@ -6,7 +6,26 @@
 #include <string.h>
 #include <sqlite3.h>
 #include <openssl/sha.h>
+#ifdef _WIN32
+#include <windows.h>  // For Sleep on Windows
+#else
+#include <unistd.h>   // For sleep on Unix-like systems
+#endif
 #include "../data/db.h"
+
+// Clear the terminal screen
+#ifdef _WIN32
+#define CLEAR_SCREEN() system("cls")
+#else
+#define CLEAR_SCREEN() system("clear")
+#endif
+
+// Pause display for 2 seconds
+#ifdef _WIN32
+#define PAUSE_DISPLAY() Sleep(2000)  // Sleep in milliseconds
+#else
+#define PAUSE_DISPLAY() sleep(2)     // sleep in seconds
+#endif
 
 // Authentication functions (auth.c)
 int login_user(sqlite3 *db);
@@ -14,5 +33,6 @@ void register_user(sqlite3 *db);
 
 // Account management functions (accounts.c)
 void account_menu(sqlite3 *db, int user_id);
+void create_account(sqlite3 *db, int user_id);
 
 #endif
